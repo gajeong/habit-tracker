b7fe86f8730421ffc74b8975f499f161769fa24e
gajeong <rlarkwjd127@gmail.com>
2022-01-31 23:35:24 +0900

    state 설정 및 setState를 이용해 state변경

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
+import '@fortawesome/fontawesome-free/js/all.js' //fontawesome을 플러그인화 
 
 ReactDOM.render(
   <React.StrictMode>
