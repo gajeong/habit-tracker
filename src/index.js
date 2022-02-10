c5f095ce7f84d78b06d33c80c6b7157a305cce75
gajeong <rlarkwjd127@gmail.com>
2022-02-10 23:30:43 +0900

    React Hook 컴포넌트형으로 바꿔보기

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './app';
-import '@fortawesome/fontawesome-free/js/all.js' //fontawesome을 플러그인화 
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+//import App from './app';
+import "@fortawesome/fontawesome-free/js/all.js"; //fontawesome을 플러그인화
+import SimpleHabit from "./components/simpleHabit";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SimpleHabit />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
